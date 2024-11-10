90b8d5869e1d1c627eed033844f0504406701936
Andre Santiago-Neyra <125695806+Andre-asn@users.noreply.github.com>
2024-11-10 17:45:22 -0500

    Updates
    + About me updated
    + Projects updated

--- pages/about.js
+++ pages/about.js
@@ -13,10 +13,10 @@ const About = () => {
               <div class="mb-auto mt-auto max-w-lg">
               <h2 className="text-6xl font-outfit font-normal text-white">A little about <span className="text-[#FCA311] font-semibold">me</span></h2>
               <p class="break-words mb-3 text-2xl pt-8 text-black md:text-[#E5E5E5]">
-                I&apos;m a student at Montclair State University, and I&apos;ll receive a Bachelor of Science in Computer Science in 2023. I was born and raised in Queens before relocating to New Jersey in 2013.
-                I went to Fort Lee High School, where I took an introductory class that exposed me to Visual Basic and helped me find my love of coding.
-                Since then, I&apos;ve continued to explore my passion in computer science by taking classes in a variety of programming languages.
-                I like to spend my free time traveling, eating new things, and catching up with loved ones. I&apos;m eager to find out where my career takes me and look forward to all the opportunities that lie ahead.</p>
+                I&apos;m a student at Stevens Institute of Tech, and I&apos;ll receive a Masters of Science in Software Engineering in 2025. I was born and raised in Queens before relocating to New Jersey in 2013.
+                I went to Fort Lee High School, where I took an introductory class that exposed me to Visual Basic and helped me find my passion of coding/development.
+                Since then, I&apos;ve continued to explore computer science by taking classes about OOP, learning system design, and expanding my knowledge of databases
+                I like to spend my free time wandering the city, eating new things, and catching up with loved ones. I&apos;m eager to find out where my career takes me and look forward to all the opportunities that lie ahead.</p>
               </div>
             </div>
           </div>
--- pages/works.js
+++ pages/works.js
@@ -11,11 +11,11 @@ const Works = () => {
     <div class="backdrop-blur-[1rem] bg-white/30 p-6 rounded-md shadow-md flex flex-col items-center max-w-[400px]">
       <h2 class="shadow-xl py-4 text-2xl text-white bg-[#14213D] w-full text-center mb-4">Anime Figures eCommerce</h2>
       <p class="text-xl break-words text-[#E5E5E5] text-center">
-        First biggest project I ever created. It was made during my time in college for a final in a database class. Currently in the process of transferring the database to MongoDB since I had originally made it in a school-based database that I no longer have access to. Keeping all the work original, exactly how it was when I submitted it, so it is not mobile responsive.
+        First biggest project I ever created. It was made during my time in college for a final in a database class. Currently in the process of transferring the database to MongoDB since I had originally made it in a school-based database that I no longer have access to, however you may view the source code on Github. Keeping all the work original, it is not mobile responsive.
       </p>
-      <form action="" target="_blank">
-        <button disabled type="submit" className="shadow-lg ring-white ring-1 hover:bg-red-600 transition ease-in-out text-white text-lg font-bold mt-6 px-6 py-3 rounded">
-          Under Construction
+      <form action="https://github.com/Andre-asn/ecommerce-proj" target="_blank">
+        <button disabled type="submit" className="shadow-lg ring-white ring-1 hover:bg-[#FCA311] transition ease-in-out text-white text-lg font-bold mt-6 px-6 py-3 rounded">
+          View
         </button>
       </form>
     </div>
@@ -26,6 +26,17 @@ const Works = () => {
       </p>
       <form action="https://karaokemock.vercel.app/" target="_blank">
         <button type="submit" className="shadow-lg ring-white ring-1 hover:bg-[#FCA311] transition ease-in-out text-white text-lg font-bold mt-6 px-6 py-3 rounded">
+          View
+        </button>
+      </form>
+    </div>
+    <div class="backdrop-blur-[1rem] bg-white/30 p-6 rounded-md shadow-md flex flex-col items-center max-w-[400px]">
+      <h2 class="shadow-xl py-4 text-2xl text-white bg-[#14213D] w-full text-center mb-4">Nutrition AI</h2>
+      <p class="text-xl break-words text-[#E5E5E5] text-center">
+        Current group project in my Agile Methods class. Working with a group of talented students to create an app that recognizes pictures of food and returns nutritional facts, along with added features like profiles, friends list, and more. Tech stack includes React + Vite, Node.js, Gemini API, and NeDB.
+      </p>
+      <form action="https://github.com/danzam284/NutritionAI" target="_blank">
+        <button disabled type="submit" className="shadow-lg ring-white ring-1 hover:bg-[#FCA311] transition ease-in-out text-white text-lg font-bold mt-6 px-6 py-3 rounded">
           View
         </button>
       </form>
